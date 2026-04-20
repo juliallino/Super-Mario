9c92132299cc34d48a8baff15081faee8027f9eb
juliallino <linojulia189@gmail.com>
2026-04-19 23:34:22 -0300

    slides finalizados

--- cenes/player.gd
+++ cenes/player.gd
@@ -3,17 +3,20 @@ extends CharacterBody2D
 class_name Player
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 
 enum PlayerMode{
 	SMALL,
 	BIG,
 	SHOOTING
 }
+signal points_scored(points: int)
+const POINTS_LABEL_SCENE = preload("res://cenes/points_label.tscn")
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var area_collision_shape_2d = $Area2D/CollisionShape2D
 @onready var body_collision_shape_2d = $BodyCollisionShape2D
+@onready var area_2d = $Area2D
 
 @export_group("Locomotion")
 @export var run_speed_damping = 0.5
@@ -21,8 +24,16 @@ enum PlayerMode{
 @export var jump_velocity = -400.0
 @export_group("")
 
+@export_group("Stomping Enemies")
+@export var min_stomp_degree = 35
+@export var max_stomp_degree = 145
+@export var stomp_y_velocity = -150
+@export_group("")
+
 var is_jumping = false
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var player_mode = PlayerMode.SMALL
+var is_dead = false
 
 func _physics_process(delta):
 	# CORREÇÃO DA GRAVIDADE: Usamos += para acumular a queda (aceleração)
@@ -78,3 +89,47 @@ func update_animation(direction):
 		animated_sprite_2d.play(prefix + "run")
 	else:
 		animated_sprite_2d.play(prefix + "idle")
+
+
+func _on_area_2d_area_entered(area):
+	if area is Enemy:
+		handle_enemy_collision(area)
+
+func handle_enemy_collision(enemy: Enemy):
+	if enemy == null || is_dead:
+		return
+	
+	if is_instance_of(enemy, Koopa) and (enemy as Koopa).in_a_shell:
+		(enemy as Koopa).on_stomp(global_position)
+	else:
+		var angle_of_collision = rad_to_deg(position.angle_to_point(enemy.position))
+		if angle_of_collision > min_stomp_degree && max_stomp_degree > angle_of_collision:
+			enemy.die()
+			on_enemy_stomped()
+			spawn_points_label(enemy)
+		else:
+			die()
+			
+func on_enemy_stomped():
+	velocity.y = stomp_y_velocity
+
+func spawn_points_label(enemy):
+	var points_label = POINTS_LABEL_SCENE.instantiate()
+	points_label.position = enemy.position + Vector2(-20,-20)
+	get_tree().root.add_child(points_label)
+	points_scored.emit(100)
+
+func die():
+	if player_mode == PlayerMode.SMALL:
+		is_dead = true
+		animated_sprite_2d.play("small_death")
+		set_physics_process(false)
+
+		var death_tween = get_tree().create_tween()
+		death_tween.tween_property(self, "position", position + Vector2(0, -48), .5)
+		death_tween.chain().tween_property(self, "position", position + Vector2(0, 256), 1)
+		death_tween. tween_callback(func (): get_tree().reload_current_scene())
+
+func _on_death_zone_body_entered(body):
+	if body == self and not is_dead:
+		die()
