f4fbbcce882efa50dcf3ba49f84fa9bf3ab9e29f
ViniciusNascimento <viniciusn2803@gmail.com>
2026-04-22 20:27:37 -0300

    primeiro commit

--- cenes/enemy.gd
+++ cenes/enemy.gd
@@ -1,62 +1,37 @@
 extends Area2D
-
 class_name Enemy
 
-const POINTS_LABEL_SCENE = preload("res://cenes/points_label.tscn")
+@export var horizontal_speed = 20 
+@export var vertical_speed = 100
+@onready var ray_cast_2d = $RayCast2D as RayCast2D
+@onready var animated_sprite_2d = $AnimatedSprite2D as AnimatedSprite2D 
 
-@export var horizontal_speed: float = 20.0
-@export var vertical_speed: float = 100.0
-var can_turn = true
-@onready var ray_floor = $RayCastFloor
-@onready var ray_edge = $RayCastEdge
-@onready var animated_sprite_2d = $AnimatedSprite2D as AnimatedSprite2D
-
-func _process(delta):
-	position.x -= horizontal_speed * delta
-
-	# gravidade
-	if !ray_floor.is_colliding():
+func _process (delta):
+	position.x -= horizontal_speed * delta 
+	
+	if !ray_cast_2d.is_colliding():
 		position.y += vertical_speed * delta
 
-	# borda
-	if !ray_edge.is_colliding():
-		if can_turn:
-			turn()
-			can_turn = false
-	else:
-		can_turn = true
-
-	
-func turn():
-	horizontal_speed *= -1
-	ray_edge.target_position.x *= -1
-	animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h
-	
-func die():
+func _die ():
 	horizontal_speed = 0
 	vertical_speed = 0
 	animated_sprite_2d.play("dead")
-	
+
 func die_from_hit():
 	set_collision_layer_value(3, false)
 	set_collision_mask_value(3, false)
 
 	rotation_degrees = 180
-	horizontal_speed = 0
-	vertical_speed = 0
-
+	horizontal_speed = 0 
+	vertical_speed = 0 
+	
 	var die_tween = get_tree().create_tween()
-	die_tween.tween_property(self, "position", position + Vector2(0, -25), .2)
-	die_tween.chain().tween_property(self, "position", position + Vector2(0, 500), 4)
-	
-	var points_label = POINTS_LABEL_SCENE.instantiate()
-	points_label.position = self.position + Vector2(-20,-20)
-	get_tree().root.add_child(points_label)
-	
-func _on_area_entered(area):
-	if area is Koopa and (area as Koopa).in_a_shell and (area as Koopa).horizontal_speed != 0:
-		die_from_hit()
+	die_tween.tween_property(self, "position", position + Vector2(0, -25 ) ,2)
+	die.tween.chain().tween.property(self, "position", position + Vector2(0, 500) ,4
 
+func _on_area_entered(area):
+	if area is Koopa and ( area as Koopa ).in_a_shell and ( area as Koopa).horizontal_speed 
+	die_from_hit() 
+	
 func _on_visible_on_screen_notifier_2d_screen_exited():
-	if global_position.x < -200 or global_position.x > 2000:
-		queue_free()
+	queue_free()
--- cenes/goomba.gd
+++ cenes/goomba.gd
@@ -1,7 +1,1 @@
-extends Enemy
-
-func die():
-	super.die()
-	set_collision_layer_value(3, false)
-	set_collision_mask_value(1, false)
-	get_tree().create_timer(0.5).timeout.connect(queue_free)
+extends Enemy 
